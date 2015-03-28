13543a7744738362b040a479bceead6c7bca1984
Trevor Woodhouse <twoodhouse@email.arizona.edu>
2015-03-27 23:28:48 -0700

    Fleshing out Move

--- src/controller/command/Move.java
+++ src/controller/command/Move.java
@@ -1,17 +1,29 @@
 package controller.command;
 
-public class Move extends Command{
+import units.Subject;
+import utils.RNG;
 
+public class Move extends Command{
+	/*
+	 * Move Parameters
+	 * Subject, desiredLocation
+	 */
 	@Override
 	protected void executeCommand(Object[] params) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	protected void undoCommand(Object[] params) {
-		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public boolean isPossible() {
+		if (RNG.getRandom().nextDouble()*((Subject)params[0]).getGrace()>.3){
+			return true;
+		}
+		return false;
 	}
 
 }
