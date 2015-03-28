ca4d6a3e1eaf5e4690341dd81b29f4594931b5ca
Trevor Woodhouse <twoodhouse@email.arizona.edu>
2015-03-28 04:54:51 -0700

    Single Turn Stack for world
    Creation of stack which can only be used to undo during the turn of use

--- src/world/World.java
+++ src/world/World.java
@@ -10,18 +10,30 @@ import controller.command.Command;
 public class World extends Observable {
 	
 	private static Stack<Command> commandHistory;
+	private static Stack<Command> singleTurnCommandHistory;
+	private int turnCounter;
 	
 	public World() {
 		commandHistory = new Stack<Command>();
+		singleTurnCommandHistory = new Stack<Command>();
+		turnCounter = 0;
 	}
 	
 	public static void doCommand(Command todo) {
 		todo.execute();
 		commandHistory.push(todo);
+		singleTurnCommandHistory.push(todo);
 	}
 	
 	public void undoLastCommand() {
 		commandHistory.pop().undo();
+		singleTurnCommandHistory.pop().undo();
+	}
+	
+	public void goToNextTurn(){
+		turnCounter++;
+		singleTurnCommandHistory.clear();
+		
 	}
 	
 	public void addPerson(Controllable c) {
