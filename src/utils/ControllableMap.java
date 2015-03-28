db0c33e5120db4fd6b5e513661448bccc2113178
whitely <googol88@gmail.com>
2015-03-27 23:56:17 -0700

    Made it singleton

--- src/utils/ControllableMap.java
+++ src/utils/ControllableMap.java
@@ -5,26 +5,33 @@ import java.util.HashMap;
 import controller.Controllable;
 import units.Subject;
 
-public class SubjectMap {
-	private static HashMap<String, Subject> hash;
+public class ControllableMap {
+	private static volatile HashMap<String, Controllable> hash;
 	
-	public SubjectMap()
+	private ControllableMap()
 	{
 		hash = new HashMap<>();
 	}
 	
-	public void put(String id, Subject subject)
+	private static void check() {
+		if (hash == null)
+			new ControllableMap();
+	}
+	
+	public static void put(String id, Subject subject)
 	{
+		check();
 		hash.put(id, subject);
 	}
 	
-	public Subject get(String id)
+	public static Controllable get(String id)
 	{
+		check();
 		return hash.get(id);
 	}
 	
 	public static Controllable get(UUID uuid) {
-		
+		return get(uuid.get());
 	}
 
 }
