c2788d1af29032a1163b13e7caf3413563bc971e
Trevor Woodhouse <twoodhouse@email.arizona.edu>
2015-03-28 13:19:41 -0700

    Correcting Losses in ControlLink file

--- src/controller/ControlLink.java
+++ src/controller/ControlLink.java
@@ -24,18 +24,39 @@ public class ControlLink {
 	
 	public Command createSummonCommand(Controllable master, Controllable slave, Point desiredLocation){
 		Command c = new Summon();
-		Object[] obj = new Object[2];
+		Object[] obj = new Object[3];
 		obj[0]= master; 
 		obj[1] = slave;
+		obj[2] = desiredLocation;
 		c.setParameters(obj);
 		return c;
 	}
 	
 	public Command createDestroyCommand(Controllable master, Controllable slave, Point desiredLocation){
 		Command c = new Destroy();
+		Object[] obj = new Object[3];
+		obj[0]= master; 
+		obj[1] = slave;
+		obj[2] = desiredLocation;
+		c.setParameters(obj);
+		return c;
+	}
+	
+	public Command createMoveCommand(Controllable master, Point desiredLocation){
+		Command c = new Destroy();
+		Object[] obj = new Object[2];
+		obj[0]= master; 
+		obj[1] = desiredLocation;
+		c.setParameters(obj);
+		return c;
+	}
+	
+	public Command createMoveOtherCommand(Controllable master, Controllable slave, Point desiredLocation){
+		Command c = new Destroy();
 		Object[] obj = new Object[2];
 		obj[0]= master; 
 		obj[1] = slave;
+		obj[2] = desiredLocation;
 		c.setParameters(obj);
 		return c;
 	}
