ecdafbfb00fbdda4937ab0655131fba9274bdf31
whitely <googol88@gmail.com>
2015-03-28 00:26:26 -0700

    Added UUID

--- src/units/elementals/Elemental.java
+++ src/units/elementals/Elemental.java
@@ -3,12 +3,15 @@ package units.elementals;
 import java.awt.Point;
 import java.util.ArrayList;
 
+import utils.UUID;
 import controller.ControlLink;
 import controller.Controllable;
 import controller.command.Command;
 
 public class Elemental implements Controllable{
 	protected Point location;
+	private UUID id;
+	
 	
 	@Override
 	public Point getLocation() {
@@ -25,6 +28,17 @@ public class Elemental implements Controllable{
 	public ArrayList<Command> getSlaveCommands() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public void setLocation(Point location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public UUID getID() {
+		return id;
 	}
 
 }
