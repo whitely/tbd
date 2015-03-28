350febddda4d07428c840496ee77440ab0576a8a
Trevor Woodhouse <twoodhouse@email.arizona.edu>
2015-03-28 15:04:38 -0700

    Registered Keypress listeners
    Follow this pattern to add additional keypress listeners

--- src/view/TestView.java
+++ src/view/TestView.java
@@ -8,8 +8,11 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.KeyStroke;
 import javax.swing.Timer;
 
 import units.Subject;
@@ -31,6 +34,7 @@ public class TestView extends JFrame {
 	public TestView() {
 		setupModel();
 		layoutGUI();
+		registerListeners();
 	}
 	
 	private void setupModel() {
@@ -57,12 +61,11 @@ public class TestView extends JFrame {
 		});
 		
 //		setLayout(new BorderLayout());
-		
-		add(p);
-		p.setLayout(null);
-		p.setLocation(0,0);
-		p.setSize(1200,900);
-		p.add(b);
+		panel1.setLayout(null);
+		add(panel1);
+		panel1.setLocation(0, 0);
+		panel1.setSize(800,800);
+		panel1.add(b);
 		b.setLocation(50,50);
 		b.setSize(500,500);
 	}
@@ -73,9 +76,78 @@ public class TestView extends JFrame {
 		}
 	}
 	
+	private void registerListeners() {
+
+		KeyStroke left = KeyStroke.getKeyStroke("LEFT");
+		KeyStroke a = KeyStroke.getKeyStroke("A");
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(left,
+				"moveLeft");
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(a,
+				"moveLeft");
+		getRootPane().getActionMap().put("moveLeft", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				leftPress();
+			}
+		});
+
+		KeyStroke right = KeyStroke.getKeyStroke("RIGHT");
+		KeyStroke d = KeyStroke.getKeyStroke("D");
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(right,
+				"moveRight");
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(d,
+				"moveRight");
+		getRootPane().getActionMap().put("moveRight", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				rightPress();
+			}
+		});
+
+		KeyStroke up = KeyStroke.getKeyStroke("UP");
+		KeyStroke w = KeyStroke.getKeyStroke("W");
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(up,
+				"moveUp");
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(w,
+				"moveUp");
+		getRootPane().getActionMap().put("moveUp", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				upPress();
+			}
+		});
+
+		KeyStroke down = KeyStroke.getKeyStroke("DOWN");
+		KeyStroke s = KeyStroke.getKeyStroke("S");
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(down,
+				"moveDown");
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(s,
+				"moveDown");
+		getRootPane().getActionMap().put("moveDown", new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				downPress();
+			}
+		});
+	}
+	
+	public void leftPress(){
+		//TODO
+	}
+	public void rightPress(){
+		//TODO
+	}
+	public void upPress(){
+		//TODO
+	}
+	public void downPress(){
+		//TODO
+	}
+	
 	static ActionListener timerAction = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			// continuous timer code here:
+			panel1.repaint();
 			//world.update();
 		}
 	};
