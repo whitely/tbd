4fa277383f6827f1596ea7f5943c80b642a9ffea
whitely <googol88@gmail.com>
2015-03-28 21:56:39 -0700

    Last commit before major refactor.

--- src/view/UnitPanel.java
+++ src/view/UnitPanel.java
@@ -44,7 +44,9 @@ public class UnitPanel extends JPanel implements Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		System.out.println("drawingPanel received update from " + o);
+		//invalidate();
 		repaint();
+		
 	}
 
 	@Override
