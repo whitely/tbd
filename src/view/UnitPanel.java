cf0c59ce33d06721d7bb8cf24bed092be72cd376
whitely <googol88@gmail.com>
2015-03-29 05:40:45 -0700

    Moved unit writing to UnitPanel...

--- src/view/UnitPanel.java
+++ src/view/UnitPanel.java
@@ -4,26 +4,26 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
-import units.Unit;
-import utils.ControllableMap;
-import controller.Controllable;
+import units.Subject;
+import world.World;
 
 @SuppressWarnings("serial")
 public class UnitPanel extends ViewPanel {
 	@Override
 	protected void draw(Graphics2D g2) {
 		System.out.println("Drawing units...");
-		Collection<Controllable> controllables = ControllableMap.getVals();
-		for (Controllable c : controllables){
+		
+		ArrayList<Subject> subjects = World.getSubjects();
+		
+		for (Subject s : subjects) {
 			try {
-				Unit u = (Unit)c;
-				BufferedImage img = ImageIO.read(new File(u.getAssetPath()));
-				g2.drawImage(img,u.getLocation().x, u.getLocation().y, u.getWidth(), u.getHeight(), null);
+				BufferedImage img = ImageIO.read(new File(s.getAssetPath()));
+				g2.drawImage(img, s.getLocation().x, s.getLocation().y, s.getWidth(), s.getHeight(), null);
 			} catch (IOException ioEx) { ioEx.printStackTrace(); continue; }
-		}	
+		}
 	}
 }
